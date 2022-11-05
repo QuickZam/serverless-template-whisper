fae942e6269fafb78008680f337d4387228732ac
QuickZam <111820344+QuickZam@users.noreply.github.com>
2022-11-05 12:13:01 +0530

    added translate and s3 links

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import torch
 import whisper
 import os
 import base64
+import urllib.request
 from pytube import YouTube
 from io import BytesIO
 
@@ -14,10 +15,16 @@ def inference(model_inputs:dict) -> dict:
     global model
 
     link = model_inputs.get('link', None)
-    yt = YouTube(link)
-    path = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
-    result = model.transcribe(path)
-    output = {"text":result["text"]}
+
+    if 'amazonaws' in link: 
+        path = urllib.request.urlretrieve(link, link.split('/')[-1].replace('%', '_'))[0]
+
+    else:
+        yt = YouTube(link)
+        path = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+
+    translate_options = dict(task="translate")
+    result = model.transcribe(path, **translate_options)
     os.remove(path)
 
-    return output
+    return result 
