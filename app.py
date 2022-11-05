5788abed444f36c3974f50ef128d103bce6be139
QuickZam <111820344+QuickZam@users.noreply.github.com>
2022-11-05 11:11:48 +0530

    changed to get link and output the text

--- app.py
+++ app.py
@@ -2,33 +2,22 @@ import torch
 import whisper
 import os
 import base64
+from pytube import YouTube
 from io import BytesIO
 
-# Init is ran on server startup
-# Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
     
     model = whisper.load_model("large")
 
-# Inference is ran for every server call
-# Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
     global model
 
-    # Parse out your arguments
-    mp3BytesString = model_inputs.get('mp3BytesString', None)
-    if mp3BytesString == None:
-        return {'message': "No input provided"}
-    
-    mp3Bytes = BytesIO(base64.b64decode(mp3BytesString.encode("ISO-8859-1")))
-    with open('input.mp3','wb') as file:
-        file.write(mp3Bytes.getbuffer())
-    
-    # Run the model
-    translate_options = dict(task="translate")
-    result = model.transcribe("input.mp3", **translate_options)
-#     output = {"text":result}
-    os.remove("input.mp3")
-    # Return the results as a dictionary
-    return result 
+    link = model_inputs.get('link', None)
+    yt = YouTube(link)
+    path = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+    result = model.transcribe(path)
+    output = {"text":result["text"]}
+    os.remove(path)
+
+    return output
