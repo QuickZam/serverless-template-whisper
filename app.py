fef7f6a62916e9e1d83100fd08db93f18bb74769
QuickZam <111820344+QuickZam@users.noreply.github.com>
2022-12-18 19:40:41 +0530

    added stable-ts

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ import base64
 import urllib.request
 from pytube import YouTube
 from io import BytesIO
+from stable_whisper import modify_model
 
 def init():
     global model
     
-    model = whisper.load_model("large")
+    model = whisper.load_model("large-v1")
+    modify_model(model)
 
 def inference(model_inputs:dict) -> dict:
     global model
@@ -23,8 +25,9 @@ def inference(model_inputs:dict) -> dict:
         yt = YouTube(link)
         path = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
 
-    translate_options = dict(task="translate")
+    translate_options = dict(task="translate", suppress_silence=True, ts_num=16, lower_quantile=0.05, lower_threshold=0.1)
     result = model.transcribe(path, **translate_options)
     os.remove(path)
 
     return result 
+    
